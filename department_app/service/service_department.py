3574d8422f7cb4e0466d066959bff24a2c744590
Iryna Feshchenko <gajchka@gmail.com>
2022-01-12 01:33:17 +0200

    docstring added

--- department_app/service/service_department.py
+++ department_app/service/service_department.py
@@ -1,50 +1,81 @@
+"""
+Module contains functions to work with Department (CRUD operations)
+
+Functions:
+    add_department(department)
+    edit_department(id_, department)
+    delete_department(id_)
+    average_salary(emp, dpt)
+"""
 from department_app import db
 from department_app.models.department import Department
 from department_app.models.employee import Employee
+from logging_file import logger
 
 
 def add_department(department):
-
-    dpt = Department(department)
-    db.session.add(dpt)
-    db.session.commit()
+    """
+    Adds new department to the db
+    :param department: department name
+    """
+    try:
+        dpt = Department(department)
+        db.session.add(dpt)
+        db.session.commit()
+    except:
+        logger.warning(f'Status: FAILED Action: DB add department')
 
 
 def edit_department(id_, department):
-
-    dpt = Department.query.get(id_)
-    emp = Employee.query.all()
-
-    for e in emp:
-        if e.department == dpt.name:
-            e.department = department
-    dpt.name = department
-    db.session.commit()
+    """
+    Edits department with id_
+    :param id_: department id
+    :param department: new department name
+    """
+    try:
+        dpt = Department.query.get(id_)
+        emp = Employee.query.all()
+        for e in emp:
+            if e.department == dpt.name:
+                e.department = department
+        dpt.name = department
+        db.session.commit()
+    except:
+        logger.warning(f'Status: FAILED Action: DB edit department')
 
 
 def delete_department(id_):
+    """
+    Deletes department with id_
+    :param id_: department id
+    """
+    try:
+        dpt = Department.query.get(id_)
+        emp = Employee.query.all()
+        for e in emp:
+            if e.department.name == dpt.name:
+                db.session.delete(e)
+        db.session.delete(dpt)
+        db.session.commit()
+    except:
+        logger.warning(f'Status: FAILED Action: DB delete department')
 
-    dpt = Department.query.get(id_)
+
+def average_salary():
+    """
+    Returns dict with departments and their average salary
+    :return: dict with departments and their average salary
+    """
+    dpt = Department.query.all()
     emp = Employee.query.all()
-
-    for e in emp:
-        if e.department.name == dpt.name:
-            db.session.delete(e)
-    db.session.delete(dpt)
-    db.session.commit()
-
-
-def average_salary(emp, dpt):
-
     total_salary = dict()
     avg_salary = dict()
     for d in dpt:
         total_salary[d.id] = []
     for e in emp:
         for d in dpt:
-            if e.department:
-                if e.department.name == d.name:
-                    total_salary[d.id].append(e.salary)
+            if e.department.name == d.name:
+                total_salary[d.id].append(e.salary)
     for dep, sal in total_salary.items():
         if len(sal) != 0:
             avg_salary[dep] = sum(sal)/len(sal)
