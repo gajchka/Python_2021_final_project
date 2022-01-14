9742639de12ab1f0768a7148d2fe8e8c07f1533e
Iryna Feshchenko <gajchka@gmail.com>
2022-01-14 23:34:18 +0200

    updating

--- department_app/__init__.py
+++ department_app/__init__.py
@@ -1,17 +1,33 @@
+"""
+Initializes web application and web service, contains following packages and
+modules:
+    migrations: contains migration files used to manage database schema
+    models: contains modules with Python classes describing database models
+    rest: contains modules with RESTful service implementation
+    service: contains modules with functions to work with database
+    static: contains web application static files (images)
+    templates: contains web application html templates
+    views: contains modules with views
+    tests: contains modules with unit tests
+"""
 from flask import Flask
+from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
+from config import Config
 
 db = SQLAlchemy()
+migrate = Migrate()
 
 
-def create_app():
-
+def create_app(conf=Config):
+    """
+    Creates app and merges it with database, blueprints and rest
+    :return: flask application instance
+    """
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.app_context().push()
+    app.config.from_object(conf)
     db.init_app(app)
-    #db.create_all()
+    migrate.init_app(app, db)
 
     from department_app.views.view_department import dpt_api
     from department_app.views.view_employee import emp_api
