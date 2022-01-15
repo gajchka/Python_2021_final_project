e63be93ee812f1ae0e2fc5ca07999e7c96e95fdc
Iryna Feshchenko <gajchka@gmail.com>
2022-01-16 00:21:48 +0200

    updated

---
+++ setup.py
@@ -0,0 +1,12 @@
+"""
+Setup file
+"""
+from setuptools import setup, find_packages
+
+setup(
+    name='Department App',
+    description='App to manage departments and employees',
+    author='Iryna Feshchenko',
+    author_email='gajchka@gmail.com',
+    packages=find_packages()
+)
